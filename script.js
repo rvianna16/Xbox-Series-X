363450d015f0a40a8683bc730a63f9f6e75cc445
Rodrigo Vianna <rvianna@pop-os.localdomain>
2021-09-27 19:37:48 -0300

    section product style

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-import MenuMobile from "./modules/menu-mobile.js";
+import MenuMobile from './modules/menu-mobile.js';
 
 const menu = new MenuMobile('btn-mobile', 'nav');
-
 menu.init();
+
+const translateImg = () => {
+  const controlContainer = document.querySelector('.console__control__grid');
+  const windowTop = window.pageYOffset;
+  const imgCrop = controlContainer.querySelector('[data-image="crop"]');
+  const topSize = imgCrop.getBoundingClientRect().top / 16.8;
+
+  if (windowTop > 3016 && windowTop < 4600) {
+    imgCrop.style.transform = `  translate3d(-${topSize + 15}%, 0px, 0px)`;
+  }
+};
+
+document.addEventListener('scroll', translateImg);
